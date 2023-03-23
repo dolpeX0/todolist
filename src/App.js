2be47ed2813ea51d31c2367b9916cb488e001398
DolpX <93961625+dolpeX0@users.noreply.github.com>
2023-03-23 10:21:06 +0800

    initial push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+function TodoList() {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleCheck = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, checked: !todo.checked } : todo
+      )
+    );
+  };
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    if (!inputValue.trim()) return;
+    setTodos([...todos, { id: Date.now(), text: inputValue, checked: false }]);
+    setInputValue("");
+  };
+
+  const handleTodoDelete = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="TodoList-container">
+      <h1>To-Do List</h1>
+      <form onSubmit={handleFormSubmit}>
+        <input type="text" value={inputValue} onChange={handleInputChange} />
+        <button type="submit">Add</button>
+      </form>
+      <ul>
+        {todos.map((todo) => (
+          <li
+            key={todo.id}
+            className="TodoList-item"
+            style={{
+              textDecoration: todo.checked && "line-through",
+            }}
+          >
+            <input
+              type="checkbox"
+              checked={todo.checked}
+              onChange={() => handleCheck(todo.id)}
+            />
+            {todo.text}
+            <button
+              onClick={() => handleTodoDelete(todo.id)}
+              className="TodoList-deleteButton"
+            >
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
 
-export default App;
+export default TodoList;
